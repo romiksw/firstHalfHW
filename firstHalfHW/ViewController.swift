e3ff8734271425bc75e056a713a798c4f98a4246
Roman <roman.d.kozlov@gmail.com>
2019-11-13 01:38:53 +0300

    Initial Commit

---
+++ firstHalfHW/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firstHalfHW
+//
+//  Created by Roman Kozlov on 12.11.2019.
+//  Copyright © 2019 Roman Kozlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
